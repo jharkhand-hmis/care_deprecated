0b2d7e084970aca2caef9ff087bf8e135df90a11
Aakash Singh <mail@singhaakash.dev>
2026-03-16 23:09:06 +0530

    update plug conf

--- plug_config.py
+++ plug_config.py
@@ -3,12 +3,6 @@ from plugs.plug import Plug
 
 plugs = [
     Plug(
-        name="care_radiology",
-        package_name="git+https://github.com/10bedicu/care_radiology.git",
-        version="@main",
-        configs={},
-    ),
-    Plug(
         name="care_state_hmis",
         package_name="git+https://github.com/10bedicu/care_state_hmis.git",
         version="@main",
@@ -17,13 +11,13 @@ plugs = [
     Plug(
         name="abdm",
         package_name="git+https://github.com/10bedicu/care_abdm.git",
-        version="@develop",
+        version="@production",
         configs={},
     ),
     Plug(
-        name="nhcx",
-        package_name="git+https://github.com/ohcnetwork/care_nhcx.git",
-        version="@develop",
+        name="analytics",
+        package_name="git+https://github.com/ohcnetwork/analytics_plug.git",
+        version="@main",
         configs={},
     ),
     Plug(
